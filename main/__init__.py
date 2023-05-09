d7ad5b7f6642aed8b94cf868f90285d2c2c05e35
whatthejames <124553067+whatthejames@users.noreply.github.com>
2023-05-10 00:57:24 +0600

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -15,9 +15,9 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 API_ID = config("23995611", default=None, cast=int)
 API_HASH = config("a1b94f10b912c9d732c320f9c73d73dc", default=None)
 BOT_TOKEN = config("6118506109:AAF6eNFgbtzDhOo-M8r5Ksh5vl2O7CFKPlk", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+SESSION = config("BQAGJUea7xepY1GtX4HZrVoUndYe5e3AET9mlT4TnUpIkV-JQLClKANCZDnJWCgnrJPiHfqZqZDW-qvFaQ56Pca_2qds7-iSo1_lsUNU4LFJeHs4ihSupNYkRjCqDZf5re1IzH3gBd3xZAaqaABowFrA6L-RzodjuMR-ngHsQuLDCMpxC_iRAMdLHynbsF05OS4stBRtuwc-igatc5HmCN_7WlSHut0-Po-l08zJWOrcCsYpnd32fIh5np1b0yja6pVSsGK9qQOq86HFzFMVgYDbLA4maHgkOpsAAYSPXbv8RA1GuKZkNWDwNwUJqdTBgFzmIT-SQj16YdHjJFFHsp7JAAAAAU-uEBwA", default=None)
+FORCESUB = config("jammesupdates", default=None)
+AUTH = config("5631774748", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
